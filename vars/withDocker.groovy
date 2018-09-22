d36e94b49900975028974db43bfacdb08f1ae146
Khoa <khoatdd@fsoft.com.vn>
2018-09-23 02:30:34 +0700

    Added withDocker.groovy

---
+++ vars/withDocker.groovy
@@ -0,0 +1,8 @@
+#!/usr/bin/env groovy
+
+def call(Closure body) {
+    def dockerTool = tool name: 'docker', type: 'org.jenkinsci.plugins.docker.commons.tools.DockerTool'
+    withEnv(["DOCKER=${dockerTool}/bin"]) {
+        body()
+    }
+}
