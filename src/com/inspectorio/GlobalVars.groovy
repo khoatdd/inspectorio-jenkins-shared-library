15970c9e776489eba8b8b57e26a17f74ac01d1f9
Khoa <khoatdd@fsoft.com.vn>
2018-09-23 01:58:59 +0700

    Added GlobalVars.groovy

---
+++ src/com/inspectorio/GlobalVars.groovy
@@ -0,0 +1,10 @@
+#!/usr/bin/env groovy
+package com.inspectorio
+
+class GlobalVars {
+    static String CONTAINER_NAME = "devops-assignment-02"
+    static String CONTAINER_TAG  = "latest"
+    static String KUBER_TEST_CONTEXT = "TEST"
+    static String KUBER_STG_CONTEXT = "STG"
+    static String KUBER_PROD_CONTEXT = "PROD"
+}
