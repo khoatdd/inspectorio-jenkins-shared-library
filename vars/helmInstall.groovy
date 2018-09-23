ea219e4a73bb6828daddea87a500b2e504ffc537
Khoa <khoatdd@fsoft.com.vn>
2018-09-23 07:50:11 +0700

    Supported passing yaml config file for helmInstall

--- vars/helmInstall.groovy
+++ vars/helmInstall.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
-def call(String kuberConfig, String kuberContext) {
-  sh "helm install --wait --debug --kubeconfig ${kuberConfig} --kube-context ${kuberContext} helm_package"
+def call(String kuberConfig, String kuberContext, String config) {
+  sh "helm install --wait --debug --kubeconfig ${kuberConfig} --kube-context ${kuberContext} -f ${config} helm_package"
 }
