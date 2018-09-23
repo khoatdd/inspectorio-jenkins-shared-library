88cba5f777b8aa0639d7272bad833b8239430c16
Khoa <khoatdd@fsoft.com.vn>
2018-09-23 07:13:32 +0700

    Remove --verify option when running helm install

--- vars/helmInstall.groovy
+++ vars/helmInstall.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
 def call(String kuberConfig, String kuberContext) {
-  sh "helm install --wait --debug --verify --kubeconfig ${kuberConfig} --kube-context ${kuberContext} helm_package"
+  sh "helm install --wait --debug --kubeconfig ${kuberConfig} --kube-context ${kuberContext} helm_package"
 }
